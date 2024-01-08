82a35dde80a5f5698b9c83f079839ef2f2048ff7
Sean La Tourette <sean.latourette@straightfire.tech>
2024-01-07 23:42:34 -0800

    8 note to 4note

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -84,67 +84,67 @@ window.addEventListener("load", () => {
 function translateHandDistanceToNoteValue(handDistance) {
     if(handDistance >= 200 && handDistance < 215 && prevNote !== "C") {
         prevNote = "C"
-        synth.triggerAttackRelease("C4", "8n");
+        synth.triggerAttackRelease("C4", "4n");
         return "C"
     } 
     else if(handDistance >= 215 && handDistance < 230 && prevNote !== "B") {
         prevNote = "B"
-        synth.triggerAttackRelease("B3", "8n");
+        synth.triggerAttackRelease("B3", "4n");
         return "B"
     } 
     else if(handDistance >= 230 && handDistance < 245 && prevNote !== "Bb") {
         prevNote = "Bb"
-        synth.triggerAttackRelease("Bb3", "8n");
+        synth.triggerAttackRelease("Bb3", "4n");
         return "Bb"
     } 
     else if(handDistance >= 245 && handDistance < 260 && prevNote !== "A") {
         prevNote = "A"
-        synth.triggerAttackRelease("A3", "8n");
+        synth.triggerAttackRelease("A3", "4n");
         return "A"
     } 
     else if(handDistance >= 260 && handDistance < 275 && prevNote !== "Ab") {
         prevNote = "Ab"
-        synth.triggerAttackRelease("Ab3", "8n");
+        synth.triggerAttackRelease("Ab3", "4n");
         return "Ab"
     } 
     else if(handDistance >= 275 && handDistance < 300 && prevNote !== "G") {
         prevNote = "G"
-        synth.triggerAttackRelease("G3", "8n");
+        synth.triggerAttackRelease("G3", "4n");
         return "G"
     } 
     else if(handDistance >= 300 && handDistance < 315 && prevNote !== "Gb") {
         prevNote = "Gb"
-        synth.triggerAttackRelease("Gb3", "8n");
+        synth.triggerAttackRelease("Gb3", "4n");
         return "Gb"
     } 
     else if(handDistance >= 315 && handDistance < 330 && prevNote !== "F") {
         prevNote = "F"
-        synth.triggerAttackRelease("F3", "8n");
+        synth.triggerAttackRelease("F3", "4n");
         return "F"
     } 
     else if(handDistance >= 330 && handDistance < 345 && prevNote !== "E") {
         prevNote = "E"
-        synth.triggerAttackRelease("E3", "8n");
+        synth.triggerAttackRelease("E3", "4n");
         return "E"
     } 
     else if(handDistance >= 345 && handDistance < 360 && prevNote !== "Eb") {
         prevNote = "Eb"
-        synth.triggerAttackRelease("Eb3", "8n");
+        synth.triggerAttackRelease("Eb3", "4n");
         return "Eb"
     } 
     else if(handDistance >= 360 && handDistance < 375 && prevNote !== "D") {
         prevNote = "D"
-        synth.triggerAttackRelease("D3", "8n");
+        synth.triggerAttackRelease("D3", "4n");
         return "D"
     } 
     else if(handDistance >= 375 && handDistance < 400 && prevNote !== "Db") {
         prevNote = "Db"
-        synth.triggerAttackRelease("Db3", "8n");
+        synth.triggerAttackRelease("Db3", "4n");
         return "Db"
     } 
     else if(handDistance >= 400 && handDistance < 415 && prevNote !== "C") {
         prevNote = "C"
-        synth.triggerAttackRelease("C3", "8n");
+        synth.triggerAttackRelease("C3", "4n");
         return "C"
     } 
     else {
